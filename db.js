18ddb78432fa6f2f2fd13146f971fbf60218e895
Abay <kinayat.abay@mail.ru>
2021-06-16 01:20:50 +0600

    first commit

---
+++ db.js
@@ -0,0 +1,5 @@
+const {Sequelize} = require('sequelize');
+
+module.exports = new Sequelize(
+  "postgres://htwezpxumomyge:235cb092b80539ec1eff88962927a869fc0ef4fece39b621567179a869a74048@ec2-34-193-112-164.compute-1.amazonaws.com:5432/d6jcs64dh6be4u"
+)
